9f6b90951debb7d1cd92449cd0d0e681cdd5ffa5
Muhammad Umer <umer15325@gmail.com>
2024-12-16 01:41:42 +0500

    responsive header and footer

--- e-commerce/src/app/components/Footer/page.tsx
+++ e-commerce/src/app/components/Footer/page.tsx
@@ -5,23 +5,34 @@ import Link from 'next/link'
 const Footer = () => {
   return (
 <div>
-    <hr className='border-[1px] border-[#D9D9D9] w-[1420px] mb-[48px]'/>
+    <hr className='border-[1px] border-[#D9D9D9] big:w-[1420px] big:mb-[48px]'/>
 
-    <div className='w-[1420px] h-[457px] px-[100px] flex flex-col gap-[48px] '>
+    <div className='big:w-[1420px] big:h-[457px] big:px-[100px] flex flex-col big:gap-[48px] 
+    mob:p-[5%] mob:gap-[20px]'>
 
 
-        <div className='flex gap-[136px]'>
-            <div className='flex flex-col gap-[50px]'>
+        <div className='flex big:gap-[136px]
+        mob:flex-col mob:gap-[20px]
+        lap:flex-row lap:gap-[40px]'>
+            {/* Logo */}
+            <div className='flex flex-col big:gap-[50px]
+            '>
                 <div className='text-[24px] font-[700]'>Funiro.</div>
                 <div className='text-[16px] font-[400] text-[#9F9F9F]'>400 University Drive Suite 200 Coral Gables, <br /> FL 33134 USA</div>
             </div>
 
 
-    <div className='flex gap-[72px]'>
-        <div className='flex gap-[144px]'>
-            <div className='flex flex-col gap-[55px]'>
+    <div className='flex big:gap-[72px]
+    mob:flex-col mob:gap-[20px]
+    lap:flex-row lap:gap-[40px]'>
+        <div className='flex big:gap-[144px]
+        mob:justify-between
+        lap:gap-[40px]'>
+            {/* Links */}
+            <div className='flex flex-col big:gap-[55px]
+            mob:gap-[5px]'>
                 <div className='text-[16px] font-[500] text-[#9F9F9F]'>Links</div>
-                <div className='flex flex-col gap-[46px]'>
+                <div className='flex flex-col big:gap-[46px]'>
                 <Link href='/' className='text-[16px] font-[500] hover:underline'>Home</Link>
                 <Link href='/shop' className='text-[16px] font-[500] hover:underline'>Shop</Link>
                 <Link href='/blog' className='text-[16px] font-[500] hover:underline'>Blog</Link>
@@ -29,9 +40,11 @@ const Footer = () => {
                 </div>
             </div>
             
-            <div className='flex flex-col gap-[50px]'>
+            {/* Help */}
+            <div className='flex flex-col big:gap-[50px]
+            mob:gap-[5px]'>
             <div className='text-[16px] font-[500] text-[#9F9F9F]'>Help</div>
-                <div className='flex flex-col gap-[46px]'>
+                <div className='flex flex-col big:gap-[46px]'>
                     <div className='text-[16px] font-[500] hover:underline hover:cursor-pointer'>Payment Options</div>
                     <div className='text-[16px] font-[500] hover:underline hover:cursor-pointer'>Returns</div>
                     <div className='text-[16px] font-[500] hover:underline hover:cursor-pointer'>Privacy Policies</div>
@@ -39,18 +52,23 @@ const Footer = () => {
             </div>
         </div>
 
-            <div className='flex flex-col gap-[50px]'>
+                {/* News Letter */}
+            <div className='flex flex-col big:gap-[50px]
+            mob:gap-[10px]'>
                 <div className='text-[16px] font-[500] text-[#9F9F9F]'>Newsletter</div>
-                <div className='flex gap-[11px]'>
-                    <input type="text" placeholder='Enter Your Email Address' className='underline text-[14px] font-[400]'/>
+                <div className='flex gap-[11px]
+                '>
+                    <input type="text" placeholder='Enter Your Email Address' className='underline text-[14px] font-[400] w-[200px]'/>
                     <button className='underline font-[500] hover:bg-black hover:text-white'>SUBSCRIBE</button>
                 </div>
             </div>
     </div>
         </div>
 
-    <div className='flex flex-col gap-[35px]'>
-        <hr className='border-[1px] border-[#D9D9D9] w-[1240px]'/>
+            {/* Last */}
+    <div className='flex flex-col big:gap-[35px]
+    mob:gap-[20px]'>
+        <hr className='border-[1px] border-[#D9D9D9] big:w-[1240px]'/>
         <div>2023 furino. All rights reverved</div>
     </div>
 
--- e-commerce/src/app/components/Header/page.tsx
+++ e-commerce/src/app/components/Header/page.tsx
@@ -4,9 +4,14 @@ import Image from 'next/image';
 import logo from '../../public/logo.png'
 import Link from 'next/link';
 import { AiOutlineShoppingCart } from "react-icons/ai";
-import { IoIosHeartEmpty } from 'react-icons/io';
+import { IoIosHeartEmpty, IoMdMenu } from 'react-icons/io';
 import { CiSearch } from 'react-icons/ci';
 import { BsPersonExclamation } from 'react-icons/bs';
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 
 const montserrat = Montserrat({
@@ -18,18 +23,26 @@ const montserrat = Montserrat({
 const Header = () => {
   return (
 
-    <div className='big:h-[100px] pl-[54px] pt-[29px] pr-[100px] pb-[30px]'>
+    <div className='big:h-[100px] big:pl-[54px] big:pt-[29px] big:pr-[100px] big:pb-[30px]
+    mob:flex mob:justify-between mob:px-[5%] mob:items-center
+    lap:h-[80px]'>
         {/* Header */}
-        <div className='big:w-[1286px] h-[41px] flex items-center justify-between'>
+        <div className='big:w-[1286px] big:h-[41px] flex items-center justify-between
+        lap:w-[100%]'>
 
             {/* Logo */}
-            <div className='flex w-[185px] justify-between items-center'>
-                <Image src={logo} alt='LOGO' className='w-[50px] h-[32px]'/>
-                <div className={`${montserrat.className} text-[34px] font-[700]`}>Furniro</div>
+            <div className='flex big:w-[185px] justify-between items-center
+            '>
+                <Image src={logo} alt='LOGO' className='lap:w-[50px] lap:h-[32px]
+                mob:w-[30px] mob:h-[30px]'/>
+                <div className={`${montserrat.className} lap:text-[34px] font-[700]
+                  mob:text-[30px]`}>Furniro</div>
             </div>
 
             {/* Routes */}
-            <div className='flex text-[16px] font-[500] w-[430px] justify-between items-center'>
+            <div className='lap:flex text-[16px] font-[500] big:w-[430px] justify-between items-center
+            mob:hidden
+            lap:w-[300px]'>
                 <Link href='/' className='hover:underline hover:text-[#B88E2F]'>Home</Link>
                 <Link href='/shop' className='hover:underline hover:text-[#B88E2F]'>Shop</Link>
                 <Link href='/blog' className='hover:underline hover:text-[#B88E2F]'>Blog</Link>
@@ -37,13 +50,40 @@ const Header = () => {
             </div>
 
             {/* Icons */}
-            <div className='flex w-[247px] justify-between items-center'>
+            <div className='lap:flex big:w-[247px] justify-between items-center
+            mob:hidden
+            lap:w-[150px]'>
                 <BsPersonExclamation className='w-[28px] h-[28px] hover:text-[#B88E2F] hover:cursor-pointer'/>
                 <CiSearch  className='w-[28px] h-[28px] hover:text-[#B88E2F] hover:cursor-pointer'/>
                 <IoIosHeartEmpty  className='w-[28px] h-[28px] hover:text-[#B88E2F] hover:cursor-pointer'/>
                 <Link href='/cart'><AiOutlineShoppingCart  className='w-[28px] h-[28px] hover:text-[#B88E2F] hover:cursor-pointer'/></Link>
             </div>
         </div>
+
+        <input type="text" placeholder='Search' className='mob:hidden lap:hidden
+        tab:flex h-[80%] px-[2%] border-[2px] border-black'/>
+
+          <Sheet>
+          <SheetTrigger>
+              <IoMdMenu className='w-[30px] h-[30px]
+              lap:hidden'/>
+          </SheetTrigger>
+
+          <SheetContent className='flex flex-col gap-[16px] w-[65%]
+          tab:w-[40%]'>
+            <input type="text" placeholder='Search' className='border-[2px] border-black px-[5%] w-[80%]
+            tab:hidden'/>
+            <Link href='/'>Home</Link>
+            <Link href='/shop'>Shop</Link>
+            <Link href='/blog'>Blog</Link>
+            <Link href='/contact'>Contact</Link>
+            <hr className='border border-black'/>
+            <Link href='#'>My Account</Link>
+            <Link href='#'>Liked</Link>
+            <Link href='/cart'>Cart</Link>
+          </SheetContent>
+          </Sheet>
+
 
     </div>
   )
